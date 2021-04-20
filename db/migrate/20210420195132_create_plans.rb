9e80cc6aaee231e9be830b0857e7fb31e2b7ed23
Alex <alex96ford19@gmail.com>
2021-04-20 20:53:04 +0100

    Changed body column to json for a table to be added.

--- db/migrate/20210420195132_create_plans.rb
+++ db/migrate/20210420195132_create_plans.rb
@@ -2,7 +2,7 @@ class CreatePlans < ActiveRecord::Migration[6.1]
   def change
     create_table :plans do |t|
       t.string :title
-      t.string :body
+      t.json :body
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_213034) do
+ActiveRecord::Schema.define(version: 2021_04_20_195132) do
 
   create_table "plans", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.json "body"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
