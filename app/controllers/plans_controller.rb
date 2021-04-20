e02dd958b5f35695519528b0605c1f04bc4128c2
Alex <alex96ford19@gmail.com>
2021-04-20 13:48:57 +0100

    Added the controler and form to create a plan.

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,9 +1,34 @@
 class PlansController < ApplicationController
 
+    # GET /plans
+    def index
+        @plans = Plan.all
+    end
+
     # GET /plans/new
     def new
-      
+      @plan = Plan.new
     end
+
+    # POST /plans/create
+    def create
+        @plan = Plan.new(plan_params)
+        @plan[:user_id] = session[:user_id]
+
+        if @plan.save
+            flash[:success] = "Plan saved."
+            redirect_to '/home'
+        else
+            flash[:danger] = "There was a problem saving the plan."
+            redirect_to 'plans/new'
+        end
+    end
+
+    private
+        # Only allow a list of trusted parameters through.
+        def plan_params
+        params.require(:plan).permit(:title, :body, :id)
+        end
  
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'sessions#login'
   resources :users
+  resources :plans
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#login'
   get '/logout', to: 'sessions#logout'
   get '/home', to: 'sessions#home'
-  # plans
-  get '/plans/new', to: 'plans#new'
+  
 
 end
